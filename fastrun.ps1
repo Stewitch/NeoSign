d118e6f18fb1ddd3b08282aa6cc7d4b74b936710
Stewitch <2207582235@qq.com>
2025-12-25 09:40:15 +0800

    chore: add fastrun.example for local fast run server setup; remove fastrun.ps1

--- fastrun.ps1
+++
@@ -1,5 +0,0 @@
-.\.venv\Scripts\activate.ps1
-uv run .\manage.py makemigrations
-uv run .\manage.py migrate
-uv run .\manage.py compilemessages -l en
-uv run .\manage.py runserver_plus 0.0.0.0:8443 --cert-file=./localhost+2.pem --key-file=./localhost+2-key.pem
